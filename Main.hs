9075803e9b4e10e3930f2fa49f630cb41cada689
Alexandru Scvortov <scvalex@gmail.com>
2012-02-11 20:12:43 +0000

    don't mutate on widget resize

--- Main.hs
+++ Main.hs
@@ -36,7 +36,7 @@ main = do
 
   let params = SkylineParameters { getMutationRate = 0.05 }
   sky <- spawn params
-  state <- newMVar (sky, params)
+  stateMV <- newMVar (sky, params)
 
   canvas <- drawingAreaNew
   canvasF <- frameNew
@@ -44,12 +44,13 @@ main = do
   set evoBox [containerChild := canvasF]
   set evoBox [boxChildPacking canvasF := PackNatural]
   widgetSetSizeRequest canvas 480 200
-  onExpose canvas $ const (updateCanvas canvas state)
+  onExpose canvas $ const (updateCanvas canvas stateMV)
 
   window `on` keyPressEvent $ tryEvent $ do
          "space" <- eventKeyName
          liftIO $ do
            (w, h) <- widgetGetSize canvas
+           mutateOnce stateMV
            widgetQueueDrawArea canvas 0 0 w h
 
   widgetShowAll window
@@ -57,19 +58,23 @@ main = do
   mainGUI
 
 updateCanvas :: DrawingArea -> MVar State -> IO Bool
-updateCanvas canvas state = do
+updateCanvas canvas stateMV = do
   win <- widgetGetDrawWindow canvas
   (width, height) <- widgetGetSize canvas
-  modifyMVar_ state $ \s ->
-      renderWithDrawable win (buildingC width height s)
+  state <- readMVar stateMV
+  renderWithDrawable win (drawSkyline width height state)
   return True
 
-buildingC :: Int -> Int -> State -> Render State
-buildingC width height (sky, params) = do
+mutateOnce :: MVar State -> IO ()
+mutateOnce stateMV = do
+  modifyMVar_ stateMV $ \(sky, params) -> do
+                       sky' <- mutate params sky
+                       return (sky', params)
+
+drawSkyline :: Int -> Int -> State -> Render ()
+drawSkyline width height (sky, params) = do
   setupCanvas width height
-  sky' <- mutate params sky
   visualize params sky
-  return (sky', params)
 
 setupCanvas :: Int -> Int -> Render ()
 setupCanvas wWidth wHeight = do
