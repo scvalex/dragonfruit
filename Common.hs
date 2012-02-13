778e7543edd76ea56a13d8662b7078830a1fddb8
Alexandru Scvortov <scvalex@gmail.com>
2012-02-13 15:32:37 +0000

    constrain building generation
    Also, generate more buildings.

--- Common.hs
+++ Common.hs
@@ -1,4 +1,20 @@
 module Common where
 
+import Control.Monad.IO.Class
+import System.Random
+
 debug :: Bool
 debug = False
+
+randomDouble :: (MonadIO m) => Double -> Double -> m Double
+randomDouble lo up = liftIO $ randomRIO (lo, up)
+
+constrainedRandom :: (MonadIO m) => (a -> Bool) -> m a -> m a
+constrainedRandom cond gen = go (10 :: Int)
+    where
+      go 0 = do
+        liftIO $ putStrLn "WARNING: constrained random limit reached"
+        gen
+      go n = do
+        x <- gen
+        if cond x then return x else go (n-1)
--- Skyline.hs
+++ Skyline.hs
@@ -4,12 +4,12 @@ module Skyline (
         Skyline(..), SkylineParameters(..), Building(..)
     ) where
 
+import Common
 import Control.Applicative
 import Control.Monad
 import Control.Monad.IO.Class
 import Data.Generics
 import Graphics.Rendering.Cairo
-import System.Random
 import Types
 
 data Skyline = Skyline { getBuildings :: [Building]
@@ -22,16 +22,19 @@ instance Genotype Skyline SkylineParameters Render where
       forM_ (getBuildings sky) (visualize params)
 
     spawn params = do
-      let genD = liftIO (randomRIO (0.1, 1.0))
+      let genD = randomDouble 0.1 1.0
       rgba <- (,,,) <$> genD <*> genD <*> genD <*> genD
-      bs <- replicateM 3 (spawn params)
+      bs <- replicateM 30 (spawn params)
       return $ Skyline bs rgba
 
     mutate params sky = do
       let mutRate = getMutationRate params
       bs' <- mapM (mutate params) (getBuildings sky)
       rgba' <- everywhereM (mkM (mutateDouble mutRate)) (getBackground sky)
-      return sky { getBuildings = bs', getBackground = rgba' }
+      r <- randomDouble 0.0 1.0
+      bs'' <- if r < mutRate then (:) <$> spawn params <*> return bs'
+                             else return bs'
+      return sky { getBuildings = bs'', getBackground = rgba' }
 
 data SkylineParameters = SkylineParameters { getMutationRate :: Double }
 
@@ -43,25 +46,31 @@ data Building = Building { getDimensions :: (Double, Double)
 instance Genotype Building SkylineParameters Render where
     visualize _ b = do
       uncurry4 setSourceRGBA (getFacade b)
-      uncurry2 (rectangle (getPositionOffset b) 0.8) (getDimensions b)
+      uncurry2 (rectangle (getPositionOffset b) 0.2) (getDimensions b)
       fill
 
     spawn _ = do
-      let gen = liftIO (randomRIO (0.1, 1.0))
-      dimensions <- (,) <$> gen <*> gen
-      rgba <- (,,,) <$> gen <*> gen <*> gen <*> gen
-      pos <- liftIO (randomRIO (0.1, 7.9))
-      return $ Building dimensions rgba pos
+      let randomBuilding = do
+                let gen = randomDouble 0.1 1.0
+                dims <- (,) <$> gen <*> gen
+                rgba <- (,,,) <$> gen <*> gen <*> gen <*> gen
+                pos <- randomDouble 0.1 7.9
+                return $ Building dims rgba pos
+      constrainedRandom validBuilding randomBuilding
 
     mutate params b = do
       let mutRate = getMutationRate params
       everywhereM (mkM (mutateDouble mutRate)) b
 
+validBuilding :: Building -> Bool
+validBuilding Building {getDimensions = (w, h), getPositionOffset = pos} =
+    w > 0 && h > 0 && w + pos > 0 && w + pos <= 8.0 && h <= 0.8 && w < 2.0
+
 mutateDouble :: (MonadIO m) => Double -> Double -> m Double
 mutateDouble mutRate d = do
-  r <- liftIO $ randomRIO (0.0, 1.0)
-  d' <- liftIO $ randomRIO (0.1, 1.0)
-  return $ if r > mutRate then d else d'
+  r <- randomDouble 0.0 1.0
+  dif <- randomDouble (-0.2) 0.2
+  return $ if r > mutRate then d else d + dif
 
 uncurry2 :: (a -> b -> c) -> (a, b) -> c
 uncurry2 f (x, y) = f x y
