232d72491b8b64f8380aa5d076df7d26bd94d327
Alexandru Scvortov <scvalex@gmail.com>
2012-02-13 15:59:09 +0000

    incomplete support for multiple canvases

--- Main.hs
+++ Main.hs
@@ -11,7 +11,9 @@ import Instances ()
 import Types
 import Skyline
 
-type State = (Skyline, SkylineParameters)
+data State = State { getCurrent :: (Skyline, DrawingArea)
+                   , getHistory :: [(Skyline, DrawingArea)]
+                   , getParams  :: SkylineParameters }
 
 main :: IO ()
 main = do
@@ -37,24 +39,30 @@ main = do
   scrolledWindowAddWithViewport mainScroller evoBox
   set evoBox [containerBorderWidth := 5]
 
-  let params = SkylineParameters { getMutationRate = 0.05 }
-  sky <- spawn params
-  stateMV <- newMVar (sky, params)
-
   canvas <- drawingAreaNew
   canvasF <- frameNew
   set canvasF [containerChild := canvas]
   set evoBox [containerChild := canvasF]
   set evoBox [boxChildPacking canvasF := PackNatural]
   widgetSetSizeRequest canvas 200 100
+
+  let params = SkylineParameters { getMutationRate = 0.05 }
+  sky <- spawn params
+  stateMV <- newMVar $ State { getCurrent = (sky, canvas)
+                             , getHistory = []
+                             , getParams  = params }
+
   onExpose canvas $ const (updateCanvas canvas stateMV)
 
   window `on` keyPressEvent $ tryEvent $ do
          "space" <- eventKeyName
          liftIO $ do
-           (w, h) <- widgetGetSize canvas
            mutateOnce stateMV
-           widgetQueueDrawArea canvas 0 0 w h
+           redrawCanvas canvas
+
+  canvas `on` buttonPressEvent $ tryEvent $ do
+         LeftButton <- eventButton
+         liftIO $ newSaveCanvas stateMV
 
   widgetShowAll window
 
@@ -70,12 +78,25 @@ updateCanvas canvas stateMV = do
 
 mutateOnce :: MVar State -> IO ()
 mutateOnce stateMV = do
-  modifyMVar_ stateMV $ \(sky, params) -> do
+  modifyMVar_ stateMV $ \s@State{ getCurrent = (sky, skyCanvas)
+                                , getParams = params} -> do
                        sky' <- mutate params sky
-                       return (sky', params)
+                       return s{ getCurrent = (sky', skyCanvas)
+                               , getParams = params }
+
+newSaveCanvas :: MVar State -> IO ()
+newSaveCanvas stateMV = do
+  modifyMVar_ stateMV $ \s@State{ getCurrent = (sky, skyCanvas)
+                                , getHistory = hist
+                                , getParams  = params} -> do
+                          newSky <- spawn params
+                          return s{ getCurrent = (newSky, skyCanvas)
+                                  , getHistory = (sky, skyCanvas):hist }
+  State{getCurrent = (_, canvas)} <- readMVar stateMV
+  redrawCanvas canvas
 
 drawSkyline :: Int -> Int -> State -> Render ()
-drawSkyline width height (sky, params) = do
+drawSkyline width height State{getCurrent = (sky, _), getParams = params} = do
   setupCanvas width height
   inUserCoordinates width height $ visualize params sky
 
@@ -116,3 +137,8 @@ inUserCoordinates devWidth devHeight render =
 
 keepState :: Render t -> Render t
 keepState render = save >> render >>= \x -> restore >> return x
+
+redrawCanvas :: DrawingArea -> IO ()
+redrawCanvas canvas = do
+  (w, h) <- widgetGetSize canvas
+  widgetQueueDrawArea canvas 0 0 w h
